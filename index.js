ce8206c213a6174f3ce40074d478b6dab8d98bab
suraj2002710 <113814706+suraj2002710@users.noreply.github.com>
2023-08-15 11:43:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const corsOptions = {
         "http://localhost:3000",
         "http://localhost:3001",
         "http://localhost:3002",
+      "https://thunderous-tiramisu-60b5f2.netlify.app"
     ],
     credentials: true,
     exposedHeaders: ["set-cookie"],
